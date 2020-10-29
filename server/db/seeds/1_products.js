ecdd0365d88c671bfc536e74e8d8c5b83a3aaf2f
jgray1 <john.edwin.gray@gmail.com>
2020-10-30 09:47:52 +1300

    add product seeds

--- server/db/seeds/1_products.js
+++ server/db/seeds/1_products.js
@@ -1,29 +1,29 @@
 // This code converted our csv data into JSON
 
-const csv = require('csv-parser')
-const fs = require('fs')
-const path = require('path')
-const results = []
+// const csv = require('csv-parser')
+// const fs = require('fs')
+// const path = require('path')
+// const results = []
 
-fs.createReadStream(path.join(__dirname, './sample_products.csv'))
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    const products = results.filter(p => {
-      return p['Type'] == 'simple'
-    })
-    .map(p => {
-      return {
-        id: p.ID,
-        title: p.Name,
-        description: p.Description,
-        image: p.Images         
-      }
-    })
-     fs.writeFile(path.join(__dirname, 'products.json'), JSON.stringify(products, null, 4), () => {
-       console.log('all done')
-     })
-  }) 
+// fs.createReadStream(path.join(__dirname, './sample_products.csv'))
+//   .pipe(csv())
+//   .on('data', (data) => results.push(data))
+//   .on('end', () => {
+//     const products = results.filter(p => {
+//       return p['Type'] == 'simple'
+//     })
+//     .map(p => {
+//       return {
+//         id: p.ID,
+//         title: p.Name,, 
+//         description: p.Description,
+//         image: p.Images         
+//       }
+//     })
+//      fs.writeFile(path.join(__dirname, 'products.json'), JSON.stringify(products, null, 4), () => {
+//        console.log('all done')
+//      })
+//   }) 
 
 const products = require('./products.json')
 
