22595789e453b75753fa6edae93a1adf388cf1b1
jgray1 <john.edwin.gray@gmail.com>
2020-10-30 14:02:01 +1300

    front-end displaying data from state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,13 +1,54 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
+const imagePlaceholder = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthealmanian.com%2Fwp-content%2Fuploads%2F2019%2F01%2Fproduct_image_thumbnail_placeholder.png&f=1&nofb=1"
 class App extends React.Component {
+  
+  // React Only
+  // state = {
+  //   products: []
+  // }
+
+  componentDidMount() {
+    // fetchProducts()
+
+  }
+
   render() {
+    // React Only
+    // const products = this.state.products
+    const products = this.props.products
+
     return (
       <>
         <h1>Hello there!</h1>
+        <ul>
+           {products.map(p => {
+             return ( 
+               <li key={p.id}>
+                 <h3>{p.title}</h3>
+                 <img src={ p.image || imagePlaceholder } style={{ maxWidth: '200px'}}/>
+              </li>
+             )
+           })}
+        </ul>
       </>
     )
   }
 }
 
-export default App
+// Fancy Pants
+// function mapStateToProps({products}) {
+//   return {
+//     products
+//   }
+// }
+// or even fancier
+// const mapStateToProps(({products}) => {products})
+
+function mapStateToProps(globalState) {
+  return {
+    products: globalState.products
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ client/reducers/products.js
@@ -0,0 +1,20 @@
+const initialState = [
+  {
+  id: 1,
+  title: 'Shirt',
+  image: '',
+},
+{
+  id: 2,
+  title: 'Shoes',
+  image: '',
+}]
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
