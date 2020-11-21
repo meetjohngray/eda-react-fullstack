17cbd23684d426198866a3e5641d9bb8d5c6d922
jgray1 <john.edwin.gray@gmail.com>
2020-11-21 13:22:55 +1300

    set-up sendgrid api key and route

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const request = require('superagent')
 const router = express.Router()
 
 const ordersDb = require('../db/orders')
+
+// Put in the .env file
+const orderTeamEmailFromAddress = 'john.gray@devacademy.co.nz'
+const orderTeamEmailToAddress = 'john.gray@devacademy.co.nz'
 
 router.post('/', (req, res) => {
   const order = req.body
   ordersDb.createOrder(order)
     .then((order) =>{
       res.json(order)
+      return order
+    })
+    .then(order => {
+      // Send an email to the orders team.
+      request.post('https://api.sendgrid.com/v3/mailsend')
+        .set('Authorization', 'Bearer ' + process.env.SENDGRID_API_KEY)
+
     })
     .catch(err => {
       console.log(err)
